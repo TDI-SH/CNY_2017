a04c59afebda31748036da69188a7656beecc4f3
robberfree <RWU@CNSHAMD0008.local>
2017-01-04 18:27:52 +0800

    add character selector

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@
     INME.Vars = {
         speed: 1,
         playerKey: 'chicken_1',
+        charactorNum: 2,
+        charactorPrefix: 'chicken',
         velocity: -550,
         velocityLong: -1550,
         gravity: 2000,
@@ -122,11 +124,21 @@
         create: function () {
             this.game.add.button(385, 400, 'btnPlay', this.handleClick, this, 1, 0, 1, 0).name = 'btnPlay';
             this.game.add.button(870, 490, 'btnHelp', this.handleClick, this, 1, 0, 1, 0).name = 'btnHelp';
+            //角色选择
+            var cs = new CharacterSelector(this.game, this.getCSKeys());
+            cs.position.set((this.game.width - cs.width) >> 1, 100);
             //初始化音效
             INME.Sound = {
                 'bg': this.game.add.audio('bg', 0.5, true),
                 'collision': this.game.add.audio('collision'),
             }
+        },
+        getCSKeys: function () {
+            var arr = [];
+            for (var i = 0; i < INME.Vars.charactorNum; i++) {
+                arr.push(INME.Vars.charactorPrefix + '_' + i + '_select');
+            }
+            return arr;
         },
         handleClick: function (btn) {
             btnName = btn.name;
@@ -139,7 +151,29 @@
                     break;
             }
         }
+
     }
+    function CharacterSelector(game, keys) {
+        var padding = 30;
+        var x = 0;
+
+        var group = game.add.group();
+        group.classType = Phaser.Button;
+
+        var len = keys.length;
+        for (var i = 0; i < len; i++) {
+            var c = group.create(x, 0, keys[i]);
+            x += (c.width + padding);
+        }
+
+        function handleClick(c) {
+            console.log('hahahahhahahhahah');
+
+        }
+
+        return group;
+    }
+
     /**
      * state - InGame
      * 使用内置的tileSprite配合tilePosition属性制作背景视差滚动时,个别手机浏览器会比较卡。决定自己实现
@@ -183,21 +217,21 @@
             if ((upArrow.isDown || spaceBar.isDown || this.game.input.pointer1.isDown) && this.player.body.touching.down) {
                 this.jumpTimer = 1;
                 this.player.body.velocity.y = INME.Vars.velocity;
-            } else if ( (upArrow.isDown || spaceBar.isDown || this.game.input.pointer1.isDown) && (this.jumpTimer !=0)) {
+            } else if ((upArrow.isDown || spaceBar.isDown || this.game.input.pointer1.isDown) && (this.jumpTimer != 0)) {
                 //player is no longer on the ground, but is still holding the jump key
-                if (this.jumpTimer > 15) { 
+                if (this.jumpTimer > 15) {
                     // player has been holding jump for over 15 frames, it's time to stop him
                     this.jumpTimer = 0;
-                } else { 
+                } else {
                     // player is allowed to jump higher (not yet 15 frames of jumping)
                     this.jumpTimer++;
                     this.player.body.velocity.y = INME.Vars.velocity;
                 }
             }
-            else if (this.jumpTimer != 0) { 
+            else if (this.jumpTimer != 0) {
                 //reset this.jumpTimer since the player is no longer holding the jump key
                 this.jumpTimer = 0;
-            }     
+            }
         },
         scrollBg: function () {
             this.bg.scroll(-INME.Vars.speed * 0.3);
@@ -207,8 +241,8 @@
         //==================================================
         //===============setting up obstacles===============
         //==================================================
-        makeObstacle: function(x,y) {
-            this.block = game.add.sprite(x,y,'obstacle', Math.floor(Math.random() * 5));
+        makeObstacle: function (x, y) {
+            this.block = game.add.sprite(x, y, 'obstacle', Math.floor(Math.random() * 5));
             this.block.scale.setTo(0.8, 0.8);
             this.obstacle.add(this.block);
             game.physics.arcade.enable(this.block);
@@ -217,25 +251,25 @@
             this.block.checkWorldBounds = true;
             this.block.outOfBoundsKill = true;
             // change existing blocks to be fast
-            if ( count >= ez) { 
-                for(var i = ez-5; i< ez+4; i++){
+            if (count >= ez) {
+                for (var i = ez - 5; i < ez + 4; i++) {
                     this.obstacle.hash[i].body.velocity.x = -300;
                 }
                 this.block.body.velocity.x = -300;
             }
         },
-        dupeObstacle: function() {
-                // Randomly pick a number between 1 and 5
+        dupeObstacle: function () {
+            // Randomly pick a number between 1 and 5
             // This will be the hole position
             var height = Math.floor(Math.random() * 2) + 1;
             this.score += 1;
             // Add the 6 obstacles 
             // With one big hole at position 'hole' and 'hole + 1'
-            for (var i = 1; i < height+1; i++) {
-                this.makeObstacle(960, (game.world.height - 64)-50*i);  
-            }   
+            for (var i = 1; i < height + 1; i++) {
+                this.makeObstacle(960, (game.world.height - 64) - 50 * i);
+            }
             count++;
-            this.game.time.events.add(this.rnd.between(1000, 3000), this.dupeObstacle, this);        
+            this.game.time.events.add(this.rnd.between(1000, 3000), this.dupeObstacle, this);
         }
     }
     function ParallaxSprite(game, key, x, y) {
