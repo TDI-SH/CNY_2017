5a72c39e8be295c5f35cefc7bde0b066d7fddf52
LucasYang <lucas.yang@wlt.com>
2017-01-20 15:26:17 +0800

    add favicon, update share buttons

--- game/js/shareButton.js
+++ game/js/shareButton.js
@@ -15,7 +15,8 @@ function share_button(){
 	var facebook = document.getElementById("facebook");
 	var linkedin = document.getElementById("linkedin");
 	var weibo = document.getElementById("weibo");
-	var host_url = document.location.href;
+	var host_url_secure = "https://roosterrun.wlt.com/game/";
+	var host_url = "http://roosterrun.wlt.com/game/";
 	//console.log(host_url);
 	
 	shareDiv.addEventListener("click", openWebPage);
@@ -31,13 +32,13 @@ function share_button(){
 		
 		switch (e.target.id) {
 			case "twitter":
-				window.open(twitterShareURL+"url="+host_url);
+				window.open(twitterShareURL+"url="+host_url+"&hashtags=RoosterRun&text=Happy Chinese New Year from Williams Lea Tag");
 				break;
 			case "facebook":
-				window.open(fbShareURL+host_url);
+				window.open(fbShareURL+host_url_secure);
 				break;
 			case "linkedin":
-				window.open(linkedinShareURL+"url="+host_url);
+				window.open(linkedinShareURL+"url="+host_url_secure);
 				break;
 			case "weibo":
 				window.open(weiboShareURL+"url="+host_url);
